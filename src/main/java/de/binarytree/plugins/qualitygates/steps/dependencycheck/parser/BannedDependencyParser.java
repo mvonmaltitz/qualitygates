4ae397cd63f11c74df9bcf62cd104e49ad65a2ce
MvM <mvm@binarytree.de>
2013-11-06 11:36:19 +0100

    Refactor BannedDependencyParser

--- src/main/java/de/binarytree/plugins/qualitygates/steps/dependencycheck/parser/BannedDependencyParser.java
+++ src/main/java/de/binarytree/plugins/qualitygates/steps/dependencycheck/parser/BannedDependencyParser.java
@@ -9,9 +9,10 @@ import java.util.regex.Pattern;
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang.StringUtils;
 
+import de.binarytree.plugins.qualitygates.steps.dependencycheck.result.BannedDependencyAnalysisResult;
+
 /**
- * This class parses the given string and extracts information about found
- * banned dependencies.
+ * This class parses the given string and extracts information about found banned dependencies.
  * 
  * @author Marcel von Maltitz
  * 
@@ -21,30 +22,37 @@ public final class BannedDependencyParser {
     private BannedDependencyParser() {
     }
 
-    private static final Pattern ARTIFACT_PATTERN = Pattern
-            .compile("Found Banned Dependency: (.*:.*:.*:.*)");
+    private static final Pattern ARTIFACT_PATTERN = Pattern.compile("Found Banned Dependency: (.*:.*:.*:.*)");
 
     /**
-     * Analyzes the dependency section and transforms the result into a structured analysis result 
-     * @param content the string to be analyzed
-     * @return a structured result 
-     * @throws IOException when the log file could not be accessed or read
+     * Analyzes the dependency section and transforms the result into a structured analysis result
+     * 
+     * @param content
+     *            the string to be analyzed
+     * @return a structured result
+     * @throws IOException
+     *             when the log file could not be accessed or read
      */
-    public static BannedDependencyAnalysisResult parseDependencyAnalyzeSection(
-            String content) throws IOException {
+    public static BannedDependencyAnalysisResult parseDependencyAnalyzeSection(String content) throws IOException {
 
         BannedDependencyAnalysisResult result = new BannedDependencyAnalysisResult();
         List<String> lines = IOUtils.readLines(new StringReader(content));
         for (String line : lines) {
-            if (!StringUtils.isBlank(line)) {
-                Matcher matcher = ARTIFACT_PATTERN.matcher(line);
-                if (matcher.matches()) {
-                    String bannedDependency = matcher.group(1);
-                    result.addBannedDependency(bannedDependency);
-                }
+            analyzeLine(result, line);
+        }
+        return result;
+    }
+
+    private static void analyzeLine(BannedDependencyAnalysisResult result, String line) {
+        if (!StringUtils.isBlank(line)) {
+            Matcher matcher = ARTIFACT_PATTERN.matcher(line);
+            if (matcher.matches()) {
+                result.addBannedDependency(getDependencyAndAddToResult(result, matcher));
             }
         }
+    }
 
-        return result;
+    private static String getDependencyAndAddToResult(BannedDependencyAnalysisResult result, Matcher matcher) {
+        return matcher.group(1);
     }
 }
--- src/test/java/de/binarytree/plugins/qualitygates/steps/dependencycheck/parser/BannedDependencyParserTest.java
+++ src/test/java/de/binarytree/plugins/qualitygates/steps/dependencycheck/parser/BannedDependencyParserTest.java
@@ -1,57 +1,90 @@
 package de.binarytree.plugins.qualitygates.steps.dependencycheck.parser;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
 
 import org.junit.Test;
 
-public class BannedDependencyParserTest{
+import de.binarytree.plugins.qualitygates.steps.dependencycheck.result.BannedDependencyAnalysisResult;
 
-	private final String SECTION_15 = "\nNon matching line\nFound Banned Dependency: org.springframework:spring-support:jar:2.0.8\n" + "Found Banned Dependency: org.springframework:spring-tx:jar:2.5.6\n" + "Found Banned Dependency: org.springframework:spring-context-support:jar:2.5.6\n" + "Found Banned Dependency: org.springframework:spring-core:jar:2.5.6\n" + "Found Banned Dependency: org.springframework:spring-mock:jar:2.0.8\n" + "Found Banned Dependency: org.springframework:spring-webmvc:jar:2.5.6\n" + "Found Banned Dependency: com.unitedinternet.portal.commons.http-utils:http-utils:jar:1.2\n" + "Found Banned Dependency: org.springframework:spring-context:jar:2.5.6\n" + "Found Banned Dependency: org.springframework:spring-web:jar:2.5.6\n" + "Found Banned Dependency: org.springframework:spring-aspects:jar:2.5.6\n" + "Found Banned Dependency: org.springframework:spring-aop:jar:2.5.6\n" + "Found Banned Dependency: org.springframework:spring-jdbc:jar:2.5.6\n" + "Found Banned Dependency: org.springframework:spring-test:jar:2.5.6\n" + "Found Banned Dependency: org.springframework:spring:jar:2.5.6\n" + "Found Banned Dependency: org.springframework:spring-beans:jar:2.5.6";
-	private final String SECTION_10 = "Found Banned Dependency: org.springframework:spring-webmvc:jar:2.5.6\n" + "Found Banned Dependency: com.unitedinternet.portal.commons.http-utils:http-utils:jar:1.2\n" + "Found Banned Dependency: org.springframework:spring-context:jar:2.5.6\n" + "Found Banned Dependency: org.springframework:spring-web:jar:2.5.6\n" + "Found Banned Dependency: org.springframework:spring-aspects:jar:2.5.6\n" + "Found Banned Dependency: org.springframework:spring-aop:jar:2.5.6\n" + "Found Banned Dependency: org.springframework:spring-jdbc:jar:2.5.6\n" + "Found Banned Dependency: org.springframework:spring-test:jar:2.5.6\n" + "Found Banned Dependency: org.springframework:spring:jar:2.5.6\n" + "Found Banned Dependency: org.springframework:spring-beans:jar:2.5.6";
-	private final String SECTION_WITH_DUPLICATES = "Found Banned Dependency: org.springframework:spring-webmvc:jar:2.5.6\n" 
-	+ "Found Banned Dependency: com.unitedinternet.portal.commons.http-utils:http-utils:jar:1.2\n" 
-			+ "Found Banned Dependency: org.springframework:spring-context:jar:2.5.6\n" 
-	+ "Found Banned Dependency: org.springframework:spring-web:jar:2.5.6\n" 
-			+ "Found Banned Dependency: org.springframework:spring-aspects:jar:2.5.6\n" 
-	+ "Found Banned Dependency: org.springframework:spring-aop:jar:2.5.6\n" 
-			+ "Found Banned Dependency: org.springframework:spring-jdbc:jar:2.5.6\n" 
-			+ "Found Banned Dependency: org.springframework:spring-context:jar:2.5.6\n" 
-	+ "Found Banned Dependency: org.springframework:spring-web:jar:2.5.6\n" 
-			+ "Found Banned Dependency: org.springframework:spring-aspects:jar:2.5.6\n" 
-	+ "Found Banned Dependency: org.springframework:spring-aop:jar:2.5.6\n" 
-			+ "Found Banned Dependency: org.springframework:spring-jdbc:jar:2.5.6\n" 
-	+ "Found Banned Dependency: org.springframework:spring-test:jar:2.5.6\n" 
-			+ "Found Banned Dependency: org.springframework:spring:jar:2.5.6\n" + "Found Banned Dependency: org.springframework:spring-beans:jar:2.5.6";
-	
-	
+public class BannedDependencyParserTest {
 
-	@Test
-	public void testParserfinds15DependenciesInSection() throws IOException{
-		BannedDependencyAnalysisResult analysis = BannedDependencyParser.parseDependencyAnalyzeSection(SECTION_15); 
-		assertEquals(15, analysis.getNumberOfBannedDependencies()); 
-		
-	}
-	@Test
-	public void testParserfinds10DependenciesInSection() throws IOException{
-		BannedDependencyAnalysisResult analysis = BannedDependencyParser.parseDependencyAnalyzeSection(SECTION_10); 
-		assertEquals(10, analysis.getNumberOfBannedDependencies()); 
-	}
-	@Test
-	public void testParserEliminatesDuplicates() throws IOException{
-		BannedDependencyAnalysisResult analysis = BannedDependencyParser.parseDependencyAnalyzeSection(SECTION_WITH_DUPLICATES); 
-		assertEquals(10, analysis.getNumberOfBannedDependencies()); 
-	}
-	@Test
-	public void testGettingDependenciesReturnsListWithCorrectSizeAndContent() throws IOException{
-		BannedDependencyAnalysisResult analysis = BannedDependencyParser.parseDependencyAnalyzeSection(SECTION_WITH_DUPLICATES); 
-		assertEquals(10, analysis.getBannedDependencies().size()); 
-		assertEquals(analysis.getNumberOfBannedDependencies(), analysis.getBannedDependencies().size()); 
-		for(String dependency : analysis.getBannedDependencies()) {
-			assertFalse(dependency.contains("Found Banned")); 
-			assertTrue(dependency.matches(".*:.*:.*:.*")); 
-		}
-	}
+    private final String SECTION_15 = "\nNon matching line\nFound Banned Dependency: org.springframework:spring-support:jar:2.0.8\n"
+            + "Found Banned Dependency: org.springframework:spring-tx:jar:2.5.6\n"
+            + "Found Banned Dependency: org.springframework:spring-context-support:jar:2.5.6\n"
+            + "Found Banned Dependency: org.springframework:spring-core:jar:2.5.6\n"
+            + "Found Banned Dependency: org.springframework:spring-mock:jar:2.0.8\n"
+            + "Found Banned Dependency: org.springframework:spring-webmvc:jar:2.5.6\n"
+            + "Found Banned Dependency: com.unitedinternet.portal.commons.http-utils:http-utils:jar:1.2\n"
+            + "Found Banned Dependency: org.springframework:spring-context:jar:2.5.6\n"
+            + "Found Banned Dependency: org.springframework:spring-web:jar:2.5.6\n"
+            + "Found Banned Dependency: org.springframework:spring-aspects:jar:2.5.6\n"
+            + "Found Banned Dependency: org.springframework:spring-aop:jar:2.5.6\n"
+            + "Found Banned Dependency: org.springframework:spring-jdbc:jar:2.5.6\n"
+            + "Found Banned Dependency: org.springframework:spring-test:jar:2.5.6\n"
+            + "Found Banned Dependency: org.springframework:spring:jar:2.5.6\n"
+            + "Found Banned Dependency: org.springframework:spring-beans:jar:2.5.6";
+
+    private final String SECTION_10 = "Found Banned Dependency: org.springframework:spring-webmvc:jar:2.5.6\n"
+            + "Found Banned Dependency: com.unitedinternet.portal.commons.http-utils:http-utils:jar:1.2\n"
+            + "Found Banned Dependency: org.springframework:spring-context:jar:2.5.6\n"
+            + "Found Banned Dependency: org.springframework:spring-web:jar:2.5.6\n"
+            + "Found Banned Dependency: org.springframework:spring-aspects:jar:2.5.6\n"
+            + "Found Banned Dependency: org.springframework:spring-aop:jar:2.5.6\n"
+            + "Found Banned Dependency: org.springframework:spring-jdbc:jar:2.5.6\n"
+            + "Found Banned Dependency: org.springframework:spring-test:jar:2.5.6\n"
+            + "Found Banned Dependency: org.springframework:spring:jar:2.5.6\n"
+            + "Found Banned Dependency: org.springframework:spring-beans:jar:2.5.6";
+
+    private final String SECTION_WITH_DUPLICATES = "Found Banned Dependency: org.springframework:spring-webmvc:jar:2.5.6\n"
+            + "Found Banned Dependency: com.unitedinternet.portal.commons.http-utils:http-utils:jar:1.2\n"
+            + "Found Banned Dependency: org.springframework:spring-context:jar:2.5.6\n"
+            + "Found Banned Dependency: org.springframework:spring-web:jar:2.5.6\n"
+            + "Found Banned Dependency: org.springframework:spring-aspects:jar:2.5.6\n"
+            + "Found Banned Dependency: org.springframework:spring-aop:jar:2.5.6\n"
+            + "Found Banned Dependency: org.springframework:spring-jdbc:jar:2.5.6\n"
+            + "Found Banned Dependency: org.springframework:spring-context:jar:2.5.6\n"
+            + "Found Banned Dependency: org.springframework:spring-web:jar:2.5.6\n"
+            + "Found Banned Dependency: org.springframework:spring-aspects:jar:2.5.6\n"
+            + "Found Banned Dependency: org.springframework:spring-aop:jar:2.5.6\n"
+            + "Found Banned Dependency: org.springframework:spring-jdbc:jar:2.5.6\n"
+            + "Found Banned Dependency: org.springframework:spring-test:jar:2.5.6\n"
+            + "Found Banned Dependency: org.springframework:spring:jar:2.5.6\n"
+            + "Found Banned Dependency: org.springframework:spring-beans:jar:2.5.6";
+
+    @Test
+    public void testParserfinds15DependenciesInSection() throws IOException {
+        BannedDependencyAnalysisResult analysis = BannedDependencyParser.parseDependencyAnalyzeSection(SECTION_15);
+        assertEquals(15, analysis.getNumberOfBannedDependencies());
+
+    }
+
+    @Test
+    public void testParserfinds10DependenciesInSection() throws IOException {
+        BannedDependencyAnalysisResult analysis = BannedDependencyParser.parseDependencyAnalyzeSection(SECTION_10);
+        assertEquals(10, analysis.getNumberOfBannedDependencies());
+    }
+
+    @Test
+    public void testParserEliminatesDuplicates() throws IOException {
+        BannedDependencyAnalysisResult analysis = BannedDependencyParser
+                .parseDependencyAnalyzeSection(SECTION_WITH_DUPLICATES);
+        assertEquals(10, analysis.getNumberOfBannedDependencies());
+    }
+
+    @Test
+    public void testGettingDependenciesReturnsListWithCorrectSizeAndContent() throws IOException {
+        BannedDependencyAnalysisResult analysis = BannedDependencyParser
+                .parseDependencyAnalyzeSection(SECTION_WITH_DUPLICATES);
+        assertEquals(10, analysis.getBannedDependencies().size());
+        assertEquals(analysis.getNumberOfBannedDependencies(), analysis.getBannedDependencies().size());
+        for (String dependency : analysis.getBannedDependencies()) {
+            assertFalse(dependency.contains("Found Banned"));
+            assertTrue(dependency.matches(".*:.*:.*:.*"));
+        }
+    }
 
 }
